8bce32fd7a1499407848e50256200d99e3e38eff
Sudheesh Parambil <hellosudhee@gmail.com>
2018-11-19 08:35:10 +0400

    Basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { WatchlistComponent } from './components/watchlist/watchlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    WatchlistComponent
   ],
   imports: [
     BrowserModule
