51d657c89fe83ed383c9d19b90e9c4619f7e543c
Sudheesh Parambil <hellosudhee@gmail.com>
2018-11-19 10:52:57 +0400

    Added ng-bootstrap, bootstrap and rxjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
 import { WatchlistComponent } from './components/watchlist/watchlist.component';
+import { SearchFilterComponent } from './components/search-filter/search-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchResultsComponent,
-    WatchlistComponent
+    WatchlistComponent,
+    SearchFilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
